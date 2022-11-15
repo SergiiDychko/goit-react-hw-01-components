bddefb86553a9540a986e7a140c89bcebc1aacf9
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-11-15 23:39:20 +0200

    профіль готовий

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import css from './profile/Profile.css';
+import './App.css';
+import { Profile } from './profile/Profile';
+import user from '../user.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+      <Profile
+        avatar= {user.avatar}
+        username= {user.username}
+        tag= {user.tag}
+        location= {user.location}
+        stats= {user.stats}
+      />
   );
-};
+}
