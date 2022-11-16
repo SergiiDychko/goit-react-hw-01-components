e6399144285b372a274ce017b34d0367dd0b9d69
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-11-16 16:25:33 +0200

    створив файли для історії транзакцій

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,13 @@ import './App.css';
 import { Profile } from './profile/Profile';
 import { Statistics } from './statistics/Statistics';
 import { FriendList } from './friendList/FriendList';
+import { FriendList } from './friendList/FriendList';
 import user from '../data/user.json'
 import data from '../data/data.json'
 import friends from '../data/friends.json'
+import transactions from '../data/transactions.json'
+import { TransactionHistory } from './transactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
     <div className="container">
@@ -17,6 +21,7 @@ export const App = () => {
       />
       <Statistics stats={data} title="Upload stats" />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 }
